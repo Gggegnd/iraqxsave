e13b28e151e4472b0558bd1f65dc105822b9bfc7
Gggegnd <101717000+Gggegnd@users.noreply.github.com>
2022-05-26 14:07:10 +0300

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -15,35 +15,35 @@ async def sett(event):
     msg = await button.get_reply_message() 
     await event.delete()
     async with Drone.conversation(event.chat_id) as conv: 
-        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        xx = await conv.send_message("أرسل لي أي صورة مصغرة كرد على هذه الرسالة.")
         x = await conv.get_reply()
         if not x.media:
-            xx.edit("No media found.")
+            xx.edit("ماكو صورة مصغرة.")
         mime = x.file.mime_type
         if not 'png' in mime:
             if not 'jpg' in mime:
                 if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
+                    return await xx.edit("لا يوجد صورة مصغرة.")
         await xx.delete()
         t = await event.client.send_message(event.chat_id, 'Trying.')
         path = await event.client.download_media(x.media)
         if os.path.exists(f'{event.sender_id}.jpg'):
             os.remove(f'{event.sender_id}.jpg')
         os.rename(path, f'./{event.sender_id}.jpg')
-        await t.edit("Temporary thumbnail saved!")
+        await t.edit("تم حفظ الصورة الرمزية بنجاح ✅🔥!")
         
 @Drone.on(events.callbackquery.CallbackQuery(data="rem"))
 async def remt(event):  
     Drone = event.client            
-    await event.edit('Trying.')
+    await event.edit('مـحـاولـة 🔁.')
     try:
         os.remove(f'{event.sender_id}.jpg')
-        await event.edit('Removed!')
+        await event.edit('إزالة ❌!')
     except Exception:
-        await event.edit("No thumbnail saved.")                        
+        await event.edit("لم يتم حفظ الصورة الرمزية❌.")                        
   
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
-    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TeamDrone"
+    text = "اهلا بك عزيزي ارسل رابط وان كان خاصة ارسل رابط الدعوة 🔥🤭"
     await start_srb(event, text)
     
